d35db6cfd13a14a80f20023dc1ff82c72664557a
Luana Calisto <luanavenancio060@gmail.com>
2024-05-22 23:11:13 -0300

    code error fix

---
+++ Screens/Index.js
@@ -0,0 +1,144 @@
+import React, {useState} from "react";
+import { View, StyleSheet, Text, TextInput, TouchableOpacity, Image } from "react-native"
+
+
+export default function Index({navigation}){
+
+
+  return(
+    <View style = {styles.containerPrincipal}>
+      <View style = {styles.topBar}>
+        <Text style = {styles.title}>Lesny.IO</Text>
+      </View>
+
+          
+        <View  style = {[styles.functionTitle, styles.elevation]}>
+          <Text style = {styles.textoLogin}> Calcular IMC e TMB </Text>
+        </View>
+
+      <View style = {styles.results}>
+            <View style = {styles.tables}>
+              <View style = {[styles.contentBox, styles.sizeUp]}>
+                <Text style = {styles.textoLogin}> IMC </Text>
+                <Text style = {styles.textoResult}> 18,7 </Text>
+                <Text style = {styles.textoLogin}> Peso ideal </Text>
+              </View>
+              <View style = {styles.contentBox}>
+                <Text style = {styles.textoLogin}> TMB - Manutenção </Text>
+                <Text style = {styles.textoResult}> 2051,4 KCAL </Text>
+                <Text style = {styles.textoLogin}> Por dia </Text>
+              </View>
+
+              
+
+            </View>
+          <View style = {styles.tables}>
+            <View style = {styles.contentBox}>
+                <Text style = {styles.textoLogin}> TMB - Deficit </Text>
+                <Text style = {styles.textoResult}> 1551,4 KCAL </Text>
+                <Text style = {styles.textoLogin}> Por dia </Text>
+              </View>
+
+              <View style = {styles.contentBox}>
+                <Text style = {styles.textoLogin}> TMB - Superavit </Text>
+                <Text style = {styles.textoResult}> 2551,4 kcal </Text>
+                <Text style = {styles.textoLogin}> Por dia </Text>
+              </View>
+
+            </View>
+      </View>
+
+        <View  style = {[styles.functionTitle, styles.elevation]}>
+          <Text style = {styles.textoLogin}> Calcular BF </Text>
+        </View>
+        <View style = {[styles.contentBox, styles.sizeDown]}>
+                <Text style = {styles.textoLogin}> Gordura corporal </Text>
+                <Text style = {styles.textoResult}> 25% </Text>
+              </View>
+
+        <View  style = {[styles.functionTitle, styles.elevation]}>
+          <Text style = {styles.textoLogin}> Calcular QDR de água </Text>
+        </View>
+        <View style = {[styles.contentBox, styles.sizeDown]}>
+                <Text style = {styles.textoResult}> 2,5L </Text>
+                <Text style = {styles.textoLogin}> Por dia </Text>
+              </View>
+      
+      
+   
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  containerPrincipal:{
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#F5F5F5"
+  },
+  topBar:{
+    flexDirection: "collumn",
+    height: 70, 
+    margin: 15
+  },
+  title:{
+    fontSize: 25,
+    fontWeight: "bold",
+    margin: 20,
+    fontFamily: "Poppins Regular",
+    textAlign: "left"
+  },
+  functionTitle:{ 
+    borderRadius: 50,
+    height: 50,
+    fontSize: 20,
+    marginHorizontal: 30,
+    fontFamily: "Poppins Regular",
+    textAlign: "center",
+    elevation: 5,
+    backgroundColor : "#6495ED",
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3
+  },
+  textoLogin:{
+    fontSize: 20,
+    fontFamily: "Poppins Regular",
+    alignSelf: "center",
+    marginTop: 10
+    },
+  results:{
+    flexDirection: "column"
+  }, 
+  textoResult:{
+    fontSize: 25,
+    fontFamily: "Poppins Regular",
+    alignSelf: "center",
+    marginTop: 10, 
+    fontWeight: "bold"
+    },
+  tables:{
+    flexDirection: "row", 
+    alignSelf: "center"
+  },
+  contentBox:{
+    backgroundColor: '#FFFAFA',
+    borderRadius: 10, 
+    contentBox: "column",
+    margin: 10,
+    padding: 10, 
+    elevation: 5,
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3
+  }, 
+  sizeDown:{
+    marginHorizontal: 30
+  }, 
+  sizeUp:{
+    width: 150
+  }
+  }
+)
